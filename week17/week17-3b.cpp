bddc2163901f486cbc71e4652d0fc2b2993df1b6
nr1201 <144758508+nr1201@users.noreply.github.com>
2024-06-12 11:54:25 +0800

    week17

---
+++ week17/week17-3b.cpp
@@ -0,0 +1,12 @@
+///方法2:選擇排序法 Selection Sort (leetcode)
+void sortColors(int* nums, int numsSize) {
+    for(int i=0; i<numsSize; i++){
+        for(int j=i+1; j<numsSize; j++){
+            if(nums[i] > nums[j]){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+}
