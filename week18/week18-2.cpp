70ed3687265f7ed2840d9ef01a42713dc5fb6f51
nr1201 <144758508+nr1201@users.noreply.github.com>
2024-06-19 11:03:41 +0800

    Add files via upload

---
+++ week18/week18-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>   ///for cin, cout
+#include <vector>     ///for a[i]
+#include <algorithm>  ///sort
+using namespace std;
+int main()
+{
+	vector<int> a(10);
+	for(int i=0; i<10; i++){
+		cin >> a[i];
+	}
+	sort(a.rbegin(), a.rend() );
+	for(int i=0; i<10; i++){
+		cout << a[i] << ' ';
+	}
+}
